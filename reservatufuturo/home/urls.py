9ecdaeb276aa3c9540dd08c1215a83a0f229b931
Jorge Muñoz Rodríguez <jorgemr@protonmail.ch>
2024-11-13 14:27:21 +0100

    feat: agregar funcionalidad de inicio y cierre de sesión de usuario

--- reservatufuturo/home/urls.py
+++ reservatufuturo/home/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
+from .views import CustomLoginView
+from django.contrib.auth.views import LogoutView
+
 
 urlpatterns = [
-    path('', views.homepage, name='homepage'),
+    path("", views.homepage, name="homepage"),
+    path("login/", CustomLoginView.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
 ]
--- reservatufuturo/home/views.py
+++ reservatufuturo/home/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth.views import LoginView
+
+class CustomLoginView(LoginView):
+    template_name = 'home/login.html'
 
 def homepage(request):
     return render(request, 'home/homepage.html')
