deaf61da8b90fcd4aa7439cc016d4d4d0ac92a48
Jorge Muñoz Rodríguez <jorgemr@protonmail.ch>
2024-11-13 14:41:46 +0100

    feat: agregar funcionalidad de registro de usuario con formulario y vista

--- reservatufuturo/home/views.py
+++ reservatufuturo/home/views.py
@@ -1,8 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.views import LoginView
+from django.contrib.auth import login
+from .forms import RegistrationForm
 
 class CustomLoginView(LoginView):
     template_name = 'home/login.html'
 
 def homepage(request):
     return render(request, 'home/homepage.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.first_name = form.cleaned_data['first_name']
+            user.last_name = form.cleaned_data['last_name']
+            user.save()
+            login(request, user)
+            return redirect('homepage')
+    else:
+        form = RegistrationForm()
+    return render(request, 'home/register.html', {'form': form})
