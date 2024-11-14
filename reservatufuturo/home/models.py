f48afb07e2db74c35b783fe267c28d5e80d6b927
Juan Garcia Carballo <juangarciacarballo@gmail.com>
2024-11-14 17:27:42 +0100

    fix: change phone_number field type to only integer

--- reservatufuturo/home/models.py
+++ reservatufuturo/home/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    phone_number = models.CharField(max_length=15, blank=True)
+    phone_number = models.IntegerField(max_length=15, blank=True)
 
     def __str__(self):
         return f'{self.user.username} Profile'
