3c12cc57dbfa34a3db06188330955fc52d030ec8
Jorge Muñoz Rodríguez <jorgemr@protonmail.ch>
2024-11-15 18:35:27 +0100

    feat: habilitar el modo DEBUG en la configuración para desarrollo

--- reservatufuturo/reservatufuturo/settings.py
+++ reservatufuturo/reservatufuturo/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b*dwjcd56ipb=*i6aqjn+8zare)5ysys#sscce(!suhy$m*3w2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['reservatufuturo.onrender.com', '127.0.0.1', 'localhost']
 
 # Application definition
