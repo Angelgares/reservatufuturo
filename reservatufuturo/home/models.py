de95c5d3a890be044bdb51da6b52f857ae51299a
Juan Garcia Carballo <juangarciacarballo@gmail.com>
2024-11-15 11:22:30 +0100

    feat: add Course and Reservation models

--- reservatufuturo/home/models.py
+++ reservatufuturo/home/models.py
@@ -3,7 +3,31 @@ from django.db import models
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    phone_number = models.IntegerField(max_length=15, blank=True)
+    phone_number = models.IntegerField(blank=True)
 
     def __str__(self):
         return f'{self.user.username} Profile'
+
+class Course(models.Model):
+    name = models.CharField(max_length=100, help_text='Course name')
+    price = models.FloatField(max_length=10, help_text='Course price')
+    image = models.ImageField(upload_to='course_images', null=True, blank=True)
+    teacher = models.CharField(max_length=100)
+    capacity = models.IntegerField(help_text='Maximum number of students')
+    
+    def __str__(self):
+        return self.name
+    
+class Reservation(models.Model):
+    PAYMENT_METHODS = [
+        ('Online', 'Online'),
+        ('Cash', 'Cash'),
+        ('Pending', 'Pending')
+    ]
+    
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    paymentMethod = models.CharField(max_length=10, choices=PAYMENT_METHODS, default='Online')
+    
+    def __str__(self):
+        return f'{self.user.username} - {self.course.name}'
