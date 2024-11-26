d39fda9c2ad667a71f90323dfd867b60293a7c86
Jorge Muñoz Rodríguez <jorgemr@protonmail.ch>
2024-11-26 14:09:54 +0100

    feat: agregar pruebas para añadir y eliminar cursos del carrito

--- reservatufuturo/courses/tests.py
+++ reservatufuturo/courses/tests.py
@@ -37,30 +37,36 @@ class CourseReservationTest(TestCase):
         self.assertEqual(reservation.paymentMethod, "Cash")
         self.assertFalse(reservation.cart)
         
-    def test_user_cant_reserve_full_course(self):
+    def test_user_can_add_course_to_cart(self):
         # Autenticar al usuario
         self.client.login(username='testuser', password='password')
 
-        # Llenar el curso
-        for i in range(10):
-            Reservation.objects.create(
-                course=self.course,
-                user=self.user,
-                paymentMethod="Cash",
-                cart=False,
-            )
+        # Añadir el curso al carrito
+        response = self.client.post(f'/cart/add/{self.course.id}/')
 
-        # Intentar crear una reserva
-        reservation = Reservation.objects.create(
-            course=self.course,
-            user=self.user,
-            paymentMethod="Cash",
-            cart=False,
-        )
+        # Verificar que el curso se ha añadido al carrito
+        self.assertEqual(Reservation.objects.count(), 1)
+        reservation = Reservation.objects.first()
+        self.assertEqual(reservation.user, self.user)
+        self.assertEqual(reservation.course, self.course)
+        self.assertEqual(reservation.paymentMethod, "Pending")
+        self.assertTrue(reservation.cart)
+        
+    def test_user_can_remove_course_from_cart(self):
+        # Autenticar al usuario
+        self.client.login(username='testuser', password='password')
 
-        # Verificar que la reserva no se ha creado
-        self.assertEqual(Reservation.objects.count(), 10)
-        self.assertNotEqual(reservation.user, self.user)
-        self.assertNotEqual(reservation.course, self.course)
-        self.assertNotEqual(reservation.paymentMethod, "Cash")
-        self.assertNotEqual(reservation.cart, False)
+        # Añadir el curso al carrito
+        self.client.post(f'/cart/add/{self.course.id}/')
+        
+        # Verificar que el curso se ha añadido al carrito
+        self.assertEqual(Reservation.objects.count(), 1)
+
+        # Eliminar el curso del carrito
+        response = self.client.post(f'/cart/remove/{Reservation.objects.first().id}/')
+
+        # Verificar que el curso se ha eliminado del carrito
+        self.assertEqual(Reservation.objects.count(), 0)
+        self.assertRedirects(response, '/cart/')
+
+    
